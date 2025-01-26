ef2853a0bac0d3e4400c025563f08239025664a5
KaveenChandika <ckaveen17@gmail.com>
2025-01-26 15:22:16 +0530

    fix for cors issue

--- router/middleware/cors.go
+++ router/middleware/cors.go
@@ -4,6 +4,19 @@ import "github.com/gin-gonic/gin"
 
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		allowedOrigins := []string{
+			"http://localhost:3000",
+		}
+
+		origin := c.GetHeader("Origin")
+
+		for _, allowedOrigin := range allowedOrigins {
+			if origin == allowedOrigin {
+				c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
+				break
+			}
+		}
+
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
