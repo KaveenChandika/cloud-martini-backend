de2e23d49a845c50164a45bc9a2a88625553a814
KaveenChandika <ckaveen17@gmail.com>
2025-01-27 12:35:17 +0530

    mongo test case added

--- db/mongo_test.go
+++ db/mongo_test.go
@@ -1,8 +1,47 @@
 package db_test
 
-import "testing"
+import (
+	"cloud-martini-backend/db"
+	"testing"
 
-func TestConnectDB(t *testing.T) {
-	validURI := "mongodb+srv://Kaveen:qX10lodLpHHEDFLg@cluster1.i6vai.mongodb.net/cloud-martini"
-	invalidURI := "mongodb+srv://Kaveen:123123@cluster1.i6vai.mongodb.net/cloud-martini"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestConnectMongo(t *testing.T) {
+	// Test cases
+	tests := []struct {
+		name        string
+		uri         string
+		expectError bool
+	}{
+		{
+			name:        "Valid MongoDB URI",
+			uri:         "mongodb+srv://Kaveen:qX10lodLpHHEDFLg@cluster1.i6vai.mongodb.net/cloud-martini", // Use your valid local MongoDB URI
+			expectError: false,
+		},
+		{
+			name:        "Invalid MongoDB URI",
+			uri:         "mongodb://invalid:27017",
+			expectError: true,
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			err := db.ConnectMongo(tc.uri)
+
+			if tc.expectError {
+				assert.Error(t, err, "Expected an error but got none")
+			} else {
+				assert.NoError(t, err, "Expected no error but got one")
+				assert.NotNil(t, db.MongoClient, "MongoClient should not be nil")
+			}
+
+			// Disconnect after successful connection
+			if db.MongoClient != nil {
+				db.DisconnectMongo()
+				assert.Nil(t, db.MongoClient, "MongoClient should be nil after disconnect")
+			}
+		})
+	}
 }
