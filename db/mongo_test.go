02fd8a3bcd17f6df0ace61a45e01636219694162
KaveenChandika <ckaveen17@gmail.com>
2025-01-27 11:55:31 +0530

    sturct updated, mongo test added

---
+++ db/mongo_test.go
@@ -0,0 +1,8 @@
+package db_test
+
+import "testing"
+
+func TestConnectDB(t *testing.T) {
+	validURI := "mongodb+srv://Kaveen:qX10lodLpHHEDFLg@cluster1.i6vai.mongodb.net/cloud-martini"
+	invalidURI := "mongodb+srv://Kaveen:123123@cluster1.i6vai.mongodb.net/cloud-martini"
+}
