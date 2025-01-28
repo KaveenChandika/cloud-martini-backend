fe3fb5fc3466c98beee56e8f3139aa95ca35fde8
KaveenChandika <ckaveen17@gmail.com>
2025-01-28 16:37:34 +0530

    new changes

--- dto/user_dto.go
+++ dto/user_dto.go
@@ -5,7 +5,7 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 type Users struct {
 	ID          primitive.ObjectID `json:"_id" bson:"_id,omitempty"`
 	Name        string             `json:"name"`
-	Designation string             `json:"Designation"`
+	Designation string             `json:"designation"`
 	Email       string             `json:"email"`
 	Projects    []string           `json:"projects"`
 }
